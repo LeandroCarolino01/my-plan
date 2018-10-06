cc38f67fad3837ac3936d44828ccb0391354cb44
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-06 17:56:30 +0200

    redux set up done

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import authReducer from './authReducer'
+import projectReducer from './projectReducer'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducer({
+    auth: authReducer,
+    project: projectReducer
+});
+
+export default rootReducer
+
