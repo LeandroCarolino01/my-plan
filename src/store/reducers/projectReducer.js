8812db6a83a0538af1f46f492776b388f8efb4b6
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-07 18:18:32 +0200

    dummy data added

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id:'1', title: 'Read a book a month', content: 'all topics'},
+        {id:'2', title: 'learn anything new', content: 'doesnt matter'},
+        {id:'3', title: 'eat healthy', content: 'lots of vegetables'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
